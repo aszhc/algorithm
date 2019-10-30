a4ea749c0942043fda643366fd048a32112c4202
aszh <andrewchou666@gmail.com>
2019-10-30 14:30:40 +0800

    10.30

---
+++ leetcode_9.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if str(x) == str(x)[::-1]:
+            return True
+        else:
+            return False
+
+    
+if __name__ == "__main__":
+    s = Solution()
+    T = s.isPalindrome
