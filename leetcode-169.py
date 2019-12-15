0e0d045a7f086e8319cda3da7ce42f715009aec0
aszh <andrewchou666@gmail.com>
2019-12-15 19:19:06 +0800

    12.15

---
+++ leetcode-169.py
@@ -0,0 +1,34 @@
+#encoding=utf8
+"""
+题目： 求众数
+
+给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+
+你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+
+示例 1:
+
+输入: [3,2,3]
+输出: 3
+示例 2:
+
+输入: [2,2,1,1,1,2,2]
+输出: 2
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/majority-element
+"""
+
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        return nums[len(nums)//2]
+
+if __name__ == "__main__":
+    Sol = Solution()
+    end = Sol.majorityElement([3,2,3])
+    print(end)
