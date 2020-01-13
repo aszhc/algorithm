ddf95784e49a91a22256172ed1bcf62d27b431ad
aszh <andrewchou666@gmail.com>
2020-01-13 22:50:36 +0800

    1.13

--- test.py
+++ test.py
@@ -1,12 +1,21 @@
+'''
+@Author: your name
+@Date: 2019-11-28 18:36:26
+@LastEditTime : 2020-01-13 22:45:45
+@LastEditors  : Please set LastEditors
+@Description: In User Settings Edit
+@FilePath: /leetcode/home/zhou/algorithm/test.py
+'''
 # encoding=utf8
 
-phone = {'2': ['a', 'b', 'c'],
-         '3': ['d', 'e', 'f'],
-         '4': ['g', 'h', 'i'],
-         '5': ['j', 'k', 'l'],
-         '6': ['m', 'n', 'o'],
-         '7': ['p', 'q', 'r', 's'],
-         '8': ['t', 'u', 'v'],
-         '9': ['w', 'x', 'y', 'z']}
+# phone = {'2': ['a', 'b', 'c'],
+#          '3': ['d', 'e', 'f'],
+#          '4': ['g', 'h', 'i'],
+#          '5': ['j', 'k', 'l'],
+#          '6': ['m', 'n', 'o'],
+#          '7': ['p', 'q', 'r', 's'],
+#          '8': ['t', 'u', 'v'],
+#          '9': ['w', 'x', 'y', 'z']}
 
-print(phone['2'[0]])
+# print(phone['2'[0]])
+
