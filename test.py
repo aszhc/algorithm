a4d028b6c03cbbf2253ba0a7cfbe3f2d6dd1f57e
zhouchuang <zhouchuang0905@qq.com>
2020-02-16 02:18:02 +0800

    2.16

--- test.py
+++ test.py
@@ -1,3 +1,14 @@
-a = [1,2,3,4,5]
-a.pop()
-print(a)
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+]
+
+# matrix = [[row[i] for row in matrix] for i in range(len(matrix[0]) - 1, -1, -1)]
+#
+# print(matrix)
+#
+# for i in range(len(matrix[0]) - 1, -1, -1):
+#     print(i)
+
+for row in matrix:
+    print(row[2])
